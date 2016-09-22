7561a60e2f7f594a64eaae2071e8c316b1f4f62f
gpreddy <prazadreddy@gmail.com>
2016-09-22 07:25:26 +0530

    Update dcd.php

--- dcd.php
+++ dcd.php
@@ -1,6 +1,5 @@
 <?php   
 
-echo" <script>top.location="https://www.facebook.com/Karsol1-1769715109954175/app/1200681023325162"
-</script> ";
+echo" <script>top.location="https://www.facebook.com/Karsol1-1769715109954175/app/1200681023325162"</script> ";
 
 ?>
