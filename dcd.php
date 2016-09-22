8242d7d9364c7ce34f065791703c5bee87616fab
gpreddy <prazadreddy@gmail.com>
2016-09-22 07:18:38 +0530

    Update dcd.php

--- dcd.php
+++ dcd.php
@@ -1,14 +1,6 @@
-<html>
-	<head>
-	<title>My first PHP page</title>
-	</head>
-	<body>
+<?php   
 
-	<?php   
-phpinfo( );
+echo" <script>top.location="https://www.facebook.com/Karsol1-1769715109954175/app/1200681023325162"
+</script> ";
 
-	?>
-
-	</body>
-	</html>
-	
+?>
